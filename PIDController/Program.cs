3605b13a5c957cb132cb5935f392fda62ad3c810
reokashiyama <reokashiyama@gmail.com>
2018-09-19 14:30:40 +0900

    Fix a bug that mislog the hour as minute

--- PIDController/Program.cs
+++ PIDController/Program.cs
@@ -56,7 +56,7 @@ namespace PIDController
             double output = pid.ControlVariable(jobj, pastTimeFromLastUpdate);
 
             System.IO.StreamWriter sw_csv = new System.IO.StreamWriter(logfilepath,true,System.Text.Encoding.GetEncoding("shift_jis"));
-            int now_in_second = DateTime.Now.Hour * 60 + DateTime.Now.Second;
+            int now_in_second = DateTime.Now.Minute * 60 + DateTime.Now.Second;
             sw_csv.Write(now_in_second.ToString() + "," + pid.SetPoint.ToString() + "," + current_value + "," + output.ToString() + "\r\n");
             sw_csv.Close();
 
